5941456ea077c17f3fe8ac0abf0c7e9fb095d191
R Sharp <reynolds.sharp@gmail.com>
2023-02-28 10:00:49 -0600

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import Head from 'next/head'
+import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* FIXME: using lottie.min.js cannot run `lottie.useWebWorker(true)` */}
+      {/*<Script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.min.js" strategy='beforeInteractive'/>*/}
+      <Script src='https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.js' strategy='beforeInteractive' />
+      <Component {...pageProps} />
+    </>
+  )
 }
